a1bdcb0220147e784be7c1cafbb1e1f9c1502c21
Vachek-hooves <vachek0602@gmail.com>
2024-12-01 18:47:50 +0200

    create welcome data

---
+++ data/welcomeData.js
@@ -0,0 +1,21 @@
+export const WELCOME_DATA = [
+  {
+    id: 1,
+    title: 'Welcome to PragmaTrack',
+    quote: "Turn your goals into achievable milestones.",
+    text:'Start your journey to a more focused, productive, and fulfilling life. PragmaTrack helps you organize, track, and achieve your goals with ease and clarity.'
+  },
+  {
+    id: 2,
+    title: 'Set Your Goals',
+    quote: "Dream big, plan smart.",
+    text:'Define your goals and categorize them into personal, professional, or other areas. Break them down into manageable steps for clarity and focus.'
+  },
+  {
+    id: 3,
+    title:"Track Your Progress",
+    quote: "Stay on track, stay motivated.",
+    text:'Visualize your journey with intuitive progress trackers and completion percentages. Stay motivated as you move closer to success.'
+  },
+  
+]
